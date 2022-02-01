e2f0877fb6de59d256354c717395b0634294da12
Raquel Moreira <rakelmoreiramendonca@gmail.com>
2022-02-01 17:07:06 -0300

    GetAllvideosvon

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { CreateVideoController } from "./controllers/CreateVideoController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
+import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
+routes.put("/categories/:id", new UpdateCategoryController().handle);
 
+routes.post("/videos", new CreateVideoController().handle);
 
-export { routes }
+export { routes };
---
+++ src/services/GetAllVideosService.ts
@@ -0,0 +1,13 @@
+import { getRepository } from "typeorm";
+import { Video } from "../entities/Video";
+
+
+export class GetAllVideosService {
+    async execute() {
+        const repo = getRepository(Video);
+
+        const videos = await repo.find();
+
+        return videos;
+    }
+}
